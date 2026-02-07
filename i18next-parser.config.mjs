378db3a945750da939a709bc79e2c5973cecd06d
chujian <765781379@qq.com>
2026-02-07 18:36:23 +0800

    feat: 添加德语、西班牙语、法语翻译支持
    - 更新 i18n 配置以支持德语、西班牙语、法语
    - 添加 i18next-parser 配置文件
    - 添加德语、西班牙语、法语的翻译资源文件
    - 更新 package.json 中的 i18n 检查命令

---
+++ i18next-parser.config.mjs
@@ -0,0 +1,21 @@
+export default {
+  contextSeparator: '.',
+  createOldCatalogs: false,
+  defaultNamespace: 'common',
+  locales: ['en', 'zh', 'es', 'fr', 'de'],
+  namespaceSeparator: ':',
+  keySeparator: '.',
+  pluralSeparator: '_',
+  interpolation: {
+    prefix: '{{',
+    suffix: '}}'
+  },
+  react: {
+    componentFolderBlacklist: ['**/node_modules/**', '**/.git/**'],
+    componentNames: ['Trans', 'Translation'],
+    hookNames: ['useTranslation'],
+    i18nKeySeparator: false,
+    defaultNS: 'common'
+  },
+  output: 'src/i18n/resources/{{lng}}/{{ns}}.json'
+};
